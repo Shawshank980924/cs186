d0f4fa07cf051ec540a7b9310faba8580472afc7
Shawshank980924 <Akatsuki980924@163.com>
2022-07-10 08:01:53 +0800

    pro4 part1 task1 finished

--- src/main/java/edu/berkeley/cs186/database/concurrency/LockType.java
+++ src/main/java/edu/berkeley/cs186/database/concurrency/LockType.java
@@ -21,6 +21,33 @@ public enum LockType {
         if (a == null || b == null) {
             throw new NullPointerException("null lock type");
         }
+        //其中一个不带锁自然可以兼容
+        if(a.equals(NL)||b.equals(NL))return true;
+        //其中一个带上IS锁的话表示该node下一个或者多个子节点获取S锁，那么该节点除了X都是可以兼容的
+        //注意IS和IX也是可以兼容的只要加的X和S不是同一个子节点即可
+        else if(a.equals(IS)||b.equals(IS)){
+            return !(a.equals(X)||b.equals(X));
+        }
+        //带IX，S,X,SIX不能兼容，其他都可以
+        else if(a.equals(IX)||b.equals(IX)){
+            LockType notIX = a.equals(IX)? b:a;
+            return !(notIX.equals(S)||notIX.equals(SIX)||notIX.equals(X));
+        }
+        //其中一个带S,IX X SIX不能兼容
+        else if(a.equals(S)||b.equals(S)){
+            LockType notS = a.equals(S)?b:a;
+            return !(notS.equals(IX)||notS.equals(X)||notS.equals(SIX));
+        }
+        //其中一个带SIX，只能兼容NL或者IS
+        else if(a.equals(SIX)||b.equals(SIX)){
+            LockType notSIX = a.equals(SIX)?b:a;
+            return notSIX.equals(NL)||notSIX.equals(IS);
+        }
+        //其中一个是X，不兼容其他任何的锁
+        else if(a.equals(X)||b.equals(X)){
+            LockType notX = a.equals(X)? b:a;
+            return notX.equals(NL);
+        }
         // TODO(proj4_part1): implement
 
         return false;
@@ -53,6 +80,29 @@ public enum LockType {
         if (parentLockType == null || childLockType == null) {
             throw new NullPointerException("null lock type");
         }
+        //子节点无锁直接返回
+        if(childLockType.equals(NL))return true;
+        //父节点是NL,子节点只能是NL
+        else if(parentLockType.equals(NL)){
+            return childLockType.equals(NL);
+        }
+        //父节点是IS，子节点能赋S或者IS
+        else if(parentLockType.equals(IS)){
+            return childLockType.equals(S)||childLockType.equals(IS);
+        }
+        //父节点是IX，子节点能赋所有的lock
+        else if(parentLockType.equals(IX)){
+            return true;
+        }
+        //父节点已经加了S或者X锁，子节点就不能加任何锁了
+        else if(parentLockType.equals(S)||parentLockType.equals(X)){
+            return false;
+        }
+        //父节点是SIX，子节点能赋IX或者X
+        else if(parentLockType.equals(SIX)){
+            return childLockType.equals(IX)||childLockType.equals(X);
+        }
+
         // TODO(proj4_part1): implement
 
         return false;
@@ -68,6 +118,22 @@ public enum LockType {
         if (required == null || substitute == null) {
             throw new NullPointerException("null lock type");
         }
+        //低等级required锁升级为高等级substitute锁
+        if(required.equals(NL))return true;
+        else if(substitute.equals(X)){
+            return required.equals(X)||required.equals(S)||required.equals(IX)||required.equals(IS)||required.equals(SIX);
+        }
+        else if(substitute.equals(S)){
+            return required.equals(S)||required.equals(IS)||required.equals(IX)||required.equals(SIX);
+        }
+        else if(substitute.equals(IS)){
+            return required.equals(IS)||required.equals(SIX);
+        }
+        else if(substitute.equals(IX))return required.equals(IX)||required.equals(IS);
+        else if(substitute.equals(SIX)){
+            return required.equals(S)||required.equals(IX);
+        }
+
         // TODO(proj4_part1): implement
 
         return false;
